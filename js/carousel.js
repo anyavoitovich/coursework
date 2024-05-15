cce494bdff98078efe6f986a070c448d3cf5a331
anyavoitovich <anya.voitovich@icloud.com>
2024-05-15 11:35:13 +0300

    new photo

--- js/carousel.js
+++ js/carousel.js
@@ -1,37 +1,24 @@
-// Получаем элементы слайдера
-const slider = document.querySelector('.slider');
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
-const slides = Array.from(slider.querySelectorAll('img'));
-const slideCount = slides.length;
-let slideIndex = 0;
+let slideIndex = 1;
 
-// Устанавливаем обработчики событий для кнопок
-prevButton.addEventListener('click', showPreviousSlide);
-nextButton.addEventListener('click', showNextSlide);
+// Вызываем функцию, чтобы показать первый слайд при загрузке страницы
+showSlides(slideIndex);
 
-// Функция для показа предыдущего слайда
-function showPreviousSlide() {
-  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
-  updateSlider();
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-// Функция для показа следующего слайда
-function showNextSlide() {
-  slideIndex = (slideIndex + 1) % slideCount;
-  updateSlider();
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-// Функция для обновления отображения слайдера
-function updateSlider() {
-  slides.forEach((slide, index) => {
-    if (index === slideIndex) {
-      slide.style.display = 'block';
-    } else {
-      slide.style.display = 'none';
-    }
-  });
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("carousel-item");
+  const dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
 }
-
-// Инициализация слайдера
-updateSlider();
